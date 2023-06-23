cfa3d1a8cdfa21e85e50c3126669e26dabcfaeeb
sanjaydanyamraju9 <danyamraju.sanjaybhargav@gmail.com>
2023-06-23 17:12:37 -0500

    added css

--- src/train_evaluate.py
+++ src/train_evaluate.py
@@ -21,6 +21,7 @@ def eval_metrics(actual, pred):
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
 
+
 def train_and_evaluate(config_path):
     config = read_params(config_path)
     test_data_path = config["split_data"]["test_path"]
@@ -43,13 +44,13 @@ def train_and_evaluate(config_path):
     test_x = test.drop(target, axis=1)
 
     lr = ElasticNet(
-        alpha=alpha, 
-        l1_ratio=l1_ratio, 
+        alpha=alpha,
+        l1_ratio=l1_ratio,
         random_state=random_state)
     lr.fit(train_x, train_y)
 
     predicted_qualities = lr.predict(test_x)
-    
+
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
     print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
@@ -77,15 +78,13 @@ def train_and_evaluate(config_path):
         json.dump(params, f, indent=4)
 #####################################################
 
-
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, "model.joblib")
 
     joblib.dump(lr, model_path)
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
